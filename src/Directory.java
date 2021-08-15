5a4813c0c650f3a30f003fda62e178a9ff5a1377
bpgould <bgould@protonmail.com>
2021-08-14 23:02:55 -0400

    Making progress on building directory helper functions and constructor to take in maxFilesPerNode arguement and depth arguement and randomly generate a tree, however, this may prove to be an interesting problem in itself

--- src/Directory.java
+++ src/Directory.java
@@ -1,17 +1,42 @@
 import java.util.ArrayList;
 
 public class Directory {
-    //create utility methods to create a directory (n-ary tree) and implement the find method here
+    // the following variables will be used to generate example directory trees for testing our find api
+    int maxFilesPerNode;
+    int treeDepth;
+    /* we want static final to ensure that these extension options are immutable across directory instances and to save memory
+    by reusing the same reference
+    */
+    final static String[] acceptableFileFormats = {"png", "jpg", "xml", "txt"};
+    File directory;
 
-    public ArrayList<File> postOrderSearch(File root) {
-        ArrayList<File> matchingFiles = new ArrayList<File>();
+    Directory(int maxFilesPerNode, int treeDepth){
+        int randomNumberOfNodes = (int)(Math.random()*maxFilesPerNode);
+        //this may be a bit more complicated, let's seek online help here
+        for(int i = 0; i < randomNumberOfNodes; i++){
+            
+        }
+    }
+
+
+    /**
+     * @param root - pass the top node of the directory to do the DFS beneath, I have chosen post order
+     * @param extensionType - file extension type that you are searching for
+     * @param maxSize - maximum allowed size of the file
+     * @return returns an arraylist of Files that can be printed to the user with after overriding toString
+     */
+    public ArrayList<File> postOrderSearch(File root, String extensionType, int maxSize) {
+        ArrayList<File> matchingFiles = new ArrayList<>();
         if(root == null){
             return matchingFiles;
         }
         for(File f : root.folder){
-            postOrderSearch(f);
+            postOrderSearch(f, extensionType, maxSize);
         }
-        matchingFiles.add(root);
+        //only add files to our list if they match the search criteria
+        if(root.extension.equals(extensionType) && root.size < maxSize) {
+            matchingFiles.add(root);
+        }
         return matchingFiles;
     }
 }
